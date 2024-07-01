96d3f01cb515f47b730763e7b359e42d67234764
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-07-01 15:06:18 +0500

    Initial Commit

---
+++ ReqResApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReqResApp2
+//
+//  Created by Даниил Сивожелезов on 01.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
